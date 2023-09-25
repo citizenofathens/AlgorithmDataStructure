9f67f444cd13c3184165196fb2d3d666ff213dff
박성민 <sminpark@datastreams.co.kr>
2023-09-25 14:41:55 +0900

    test

--- programmers_야근지수.py
+++ programmers_야근지수.py
@@ -25,3 +25,40 @@ def solution(n, works):
         # 큰 값부터 처리한 뒤 1번 줄여도 다른 값보다 크다면 다시 큰값부터
 
     return answer
+
+
+import math
+import copy
+
+def solution(n, works):
+    # 야근 피로도 : 야근 시작 시점에서 남은 일의 작업량을 제곱하여 더한다
+    if len(works) ==0:
+        return 0
+    answer = 0
+    count_n = copy.copy(n)
+
+    works.sort(reverse=True)
+    # works 가 없어진다면?
+    for idx in range(len(works)+1):
+        print('beftore works', works)
+        # print('idx ,{}'.format(idx),works)
+        # print('n:',n)
+        # print(works[idx])
+        if idx == len(works):
+            idx = idx//n
+
+        if works[idx] ==0:
+            pass
+        else:
+            works[idx] -= 1
+        count_n -=1
+        print('after works', works)
+        print(idx,count_n)
+
+
+        if count_n ==0:
+            answer =  int(sum(list(map(lambda x: math.pow(x,2), works))))
+            return answer
+        # 큰 값부터 처리한 뒤 1번 줄여도 다른 값보다 크다면 다시 큰값부터
+
+    return answer
